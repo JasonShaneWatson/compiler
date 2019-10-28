83d146d9a7aaef9deadcc9dae8836b802abf02b5
aspenlynne <56941099+aspenlynne@users.noreply.github.com>
2019-10-28 17:17:15 -0500

    Update stage0.cpp
    added func for key words

--- src/stage0.cpp
+++ src/stage0.cpp
@@ -28,4 +28,18 @@ void printSymbolTable()
 {
 
 }
-
+bool Key_Id(string s)
+{
+	string key[] = {"Program", "begin", "end", "var", "conts", "integer", "boolean", "true", "false", "not"};
+	
+	for (uint i = 0; i <= sizeof(key); i++)
+	{
+		if (key[i] == token)
+		{
+			//error
+			return false;
+		}
+	}
+	
+	return true;
+}
